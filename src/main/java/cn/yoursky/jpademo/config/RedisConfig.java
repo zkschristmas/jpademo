46848c30d35e147125be63c31f49c93e6200653d
zkschristmas <schristmas@163.com>
2020-02-08 18:15:07 +0800

    redis作为查询缓存

---
+++ src/main/java/cn/yoursky/jpademo/config/RedisConfig.java
@@ -0,0 +1,36 @@
+package cn.yoursky.jpademo.config;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+
+@Configuration
+@EnableCaching
+public class RedisConfig extends CachingConfigurerSupport {
+    @Bean
+    public CacheManager cacheManager (RedisConnectionFactory factory) {
+        return RedisCacheManager.builder(factory).build();
+    }
+
+    @Bean
+    public KeyGenerator keyGenerator() {
+        return new KeyGenerator(){
+            @Override
+            public Object generate(Object target, java.lang.reflect.Method method, Object... params) {
+                StringBuffer sb = new StringBuffer();
+                sb.append(target.getClass().getName());
+                sb.append(method.getName());
+                for(Object obj:params){
+                    sb.append(obj.toString());
+                }
+                System.out.println("调用Redis生成key："+sb.toString());
+                return sb.toString();
+            }
+        };
+    }
+}
